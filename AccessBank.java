a8708c5b8b3a2002a4c0c21926081391ec83d704
Stanley <stanleyobitunwase@gmail.com>
2024-04-15 13:12:07 +0100

    oop

---
+++ AccessBank.java
@@ -0,0 +1,17 @@
+package oop;
+
+import repo15.Bank;
+
+public class AccessBank extends Bank {
+    public void accountDetail() {
+    }
+
+    public void showBankName() {
+
+    }
+
+    public void BankLocation(){
+
+    }
+    Bank bank = new AccessBank();
+}
